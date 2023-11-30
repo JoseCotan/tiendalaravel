ec8df9530c6c90287a80e4952e2790c09a39193d
José Luis Cotán <joseluis.cotan@iesdonana.org>
2023-11-30 21:15:42 +0100

    fdiajf

---
+++ routes/web.php
@@ -0,0 +1,77 @@
+<?php
+
+use App\Generico\Carrito;
+use App\Http\Controllers\ArticuloController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProfileController;
+use App\Models\Articulo;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('principal', [
+        'articulos' => Articulo::with(['iva', 'categoria'])->get(),
+        'carrito' => carrito(),
+    ]);
+})->name('principal');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/ricardo', function () {
+    return view('ricardo');
+});
+
+Route::resource('categorias', CategoriaController::class)
+    ->middleware('auth');
+
+Route::resource('articulos', ArticuloController::class);
+
+Route::get('/carrito/insertar/{id}', function ($id) {
+    $articulo = Articulo::findOrFail($id);
+    $carrito = carrito();
+    $carrito->insertar($id);
+    session()->put('carrito', $carrito);
+    return redirect()->route('principal');
+})->name('carrito.insertar')->whereNumber('id');
+
+Route::get('/carrito/vaciar', function () {
+    session()->forget('carrito');
+    return redirect()->route('principal');
+})->name('carrito.vaciar');
+
+Route::get('/carrito/eliminar/{id}', function ($id) {
+    $articulo = Articulo::findOrFail($id);
+    $carrito = carrito();
+    $carrito->eliminar($id);
+    session()->put('carrito', $carrito);
+    return redirect()->route('principal');
+})->name('carrito.eliminar')->whereNumber('id');
+
+Route::get('/carrito/aumentar/{id}', function ($id) {
+    $articulo = Articulo::findOrFail($id);
+    $carrito = carrito();
+    $carrito->aumentar($id);
+    session()->put('carrito', $carrito);
+    return redirect()->route('principal');
+})->name('carrito.aumentar')->whereNumber('id');
+
+require __DIR__.'/auth.php';
